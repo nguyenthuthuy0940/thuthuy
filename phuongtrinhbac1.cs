75e3ea9580e5da2053055bc86a5f046fef3f23be
nguyenthuthuy0940 <77652340+nguyenthuthuy0940@users.noreply.github.com>
2021-01-20 20:54:57 +0700

    Add files via upload

---
+++ phuongtrinhbac1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace phuongtrinh_bac1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhập a");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("nhập b");
+            int b = int.Parse(Console.ReadLine());
+
+                if (a == 0)
+                if (b == 0)
+                        Console.WriteLine("phương trình vô số nghiệm");
+                    else
+                        Console.WriteLine("phuương trình vô nghiệm");
+                    else
+                    {
+                        int nghiem = -b / a;
+                        Console.WriteLine("phương trình có nghiệm là x={0}", nghiem.ToString());
+                    }
+            Console.ReadKey();
+        }
+    }
+}
