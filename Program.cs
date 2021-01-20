7317751c2a07bfa1413069c5bbb36226974e47df
nguyenthuthuy0940 <77652340+nguyenthuthuy0940@users.noreply.github.com>
2021-01-20 20:55:46 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace heloo_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
